f08fda32db1a83179115d82f6c321d76afb54044
dkang417 <dkang417@gmail.com>
2018-09-19 11:37:31 -0400

    added action creator and implemented posts reducer

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+
+  
+
+    // calls for 1 time when component shows up on page
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
     render() {
+        // console.log(this.props.posts);
         return (
             <div>
-                Posts PostsIndex is here!!!
+                <h3>Posts</h3>
+                <ul className="list-group">
+                {this.renderPosts()}    
+                   </ul> 
             </div>
         );
     }
 }
 
-export default PostsIndex;
+// if we need anything from app level state we need to define this function
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+
+export default connect(mapStateToProps, {fetchPosts}) (PostsIndex);
