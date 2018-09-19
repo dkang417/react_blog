89ee69c1d03ffd02dca81d12f36d94f1ed46f207
dkang417 <dkang417@gmail.com>
2018-09-19 11:02:51 -0400

    created index action

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>
+                Posts PostsIndex is here!!!
+            </div>
+        );
+    }
+}
+
+export default PostsIndex;
